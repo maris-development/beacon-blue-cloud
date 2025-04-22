fbafbebdbf05d12ac8e470f4b5b304d10e749fab
Robin Kooyman <robin.kooyman.work@gmail.com>
2025-04-22 11:08:23 +0200

    Add merged notebooks for argo, cora-pr, cora-ts

--- notebook-examples/1.0.1 (latest)/physics/harmonization/util.py
+++ notebook-examples/1.0.1 (latest)/physics/harmonization/util.py
@@ -2,13 +2,19 @@ def coalesce_columns(columns: list[str], alias) -> dict:
     return {"function": "coalesce", "args": columns, "alias": alias}
 
 
-def function_call(function: str, columns: list[str], alias: str = None) -> dict:
-    return {"function": function, "args": columns, "alias": alias}
-
+def function_call(function: str, columns: list[str], alias: str = None, try_cast = None) -> dict:
+    if try_cast: 
+        return {"function": function, "args": columns, "alias": alias, "try_cast": try_cast}
+    else:
+        return {"function": function, "args": columns, "alias": alias}
+    
 
 def value(value, alias: str = None) -> dict:
     return {"value": value, "alias": alias}
 
 
-def column(column: str, alias: str = None) -> dict:
-    return {"column": column, "alias": alias}
+def column(column: str, alias: str = None, try_cast= None) -> dict:
+    if try_cast:
+        return {"column": column, "alias": alias, "try_cast": try_cast}
+    else: 
+        return {"column": column, "alias": alias}
